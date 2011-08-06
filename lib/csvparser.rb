51411461fbf4e6cf0b32db0fd054095a7013b513
Stefan Rohlfing <stefan.rohlfing@gmail.com>
2011-08-06 15:26:12 +0800

    added feature to choose a custom delimiter; tested csv with Chinese content

--- lib/csvparser.rb
+++ lib/csvparser.rb
@@ -1,5 +1,8 @@
+# encoding: utf-8
+# http://blog.grayproductions.net/articles/ruby_19s_three_default_encodings
 require 'parslet'
 require 'pp'
+require 'parslet/convenience'
 
 # Common Format and MIME Type for CSV Files (RFC 4180)
 # Source: http://www.ietf.org/rfc/rfc4180.txt
@@ -22,6 +25,12 @@ EOS
 
 # Parset rules accoding to the above abnf rules, except for that I don't distinguish between a header and a record. 
 class CSVParser < Parslet::Parser
+
+  @delimiter = ','
+
+  def initialize delimiter = ','
+   @delimiter = delimiter 
+  end
  
   rule(:file)        {(record.as(:row) >> newline).repeat(1)}
   # rule(:file)        {record.as(:row) >> (newline >> record.as(:row)).repeat >> newline.maybe}
@@ -34,7 +43,7 @@ class CSVParser < Parslet::Parser
   rule(:lf)          {str("\n")}
   rule(:cr)          {str("\r")}
   rule(:d_quote)     {str('"')}
-  rule(:comma)       {str(',')} 
+  rule(:comma)       {str(@delimiter)} 
 
   root(:file)
 end
@@ -57,6 +66,24 @@ csv_content2 = <<CSV
 10,20,,,,
 CSV
 
-pp CSVParser.new.parse(csv_content2)
+# semicolon (;) as the delimiter
+csv_content3 = <<CSV
+"col1";"col2";"col3";"col4";"col5";"col6"
+1;2;3;4;5;6
+10;20;;;;
+CSV
+# pp CSVParser.new.parse_with_debug(csv_content)
+# pp CSVParser.new.parse(csv_content2)
+
+pp CSVParser.new(';').parse(csv_content3)
+
+csv_content_chinese = <<CSV
+"编码","规格","单价（美元）","数量","总价（美元）"
+1,"好产品",3,2,6
+2,"豪华汽车，价格很划算",,,
+3,"“小兔”牌",4.4,10,44
+CSV
+
+pp CSVParser.new.parse(csv_content_chinese)
 
 
